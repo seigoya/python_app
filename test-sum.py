1bc9f7008d90877212d34ba1b48df3edf253bcc9
seigoya <westwing283@gmail.com>
2026-08-26 22:01:48 +0900

    2026082602commit

--- test-sum.py
+++ test-sum.py
@@ -1,9 +1,9 @@
 # あるクラスの国語テストの点数をリストに代入 --- (*1)
 points = [88, 76, 67, 43, 79, 80, 91]
-
+math_points = [30, 60, 85, 62, 80, 55, 92]
 # テストの合計を求める --- (*2)
 sum_v = 0
-for i in points:
+for i in math_points:
     sum_v += i
     print(i,"点を足して合計は", sum_v)
 
